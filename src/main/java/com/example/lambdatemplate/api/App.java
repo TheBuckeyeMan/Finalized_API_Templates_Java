ed3a32b2dc135a7fd3895ce32fdeec1560bdb7cf
Adam Maas <adamjm1220@gmail.com>
2024-10-03 12:11:39 -0400

    Template Update

--- src/main/java/com/example/lambdatemplate/api/App.java
+++ src/main/java/com/example/lambdatemplate/api/App.java
@@ -1,4 +1,4 @@
-package com.example.lambdatemplate;
+package com.example.lambdatemplate.api;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
